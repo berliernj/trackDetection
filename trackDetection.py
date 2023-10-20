9e6dbc7c94d0168f49b6c8f8a2960cf59434c45c
berliernj <berlier3@gmail.com>
2023-10-20 15:10:17 -0400

    first commit

---
+++ trackDetection.py
@@ -0,0 +1,49 @@
+import cv2
+import numpy as np
+
+# Have to hard code points of interest to transform
+def perspective_warp(img, 
+                     dst_size=(1280,720),
+                     src=np.float32([(0.43,0.65),(0.58,0.65),(0.1,1),(1,1)]),
+                     dst=np.float32([(0,0), (1, 0), (0,1), (1,1)])):
+    img_size = np.float32([(img.shape[1],img.shape[0])])
+    src = src* img_size
+    # For destination points, I'm arbitrarily choosing some points to be
+    # a nice fit for displaying our warped result 
+    # again, not exact, but close enough for our purposes
+    dst = dst * np.float32(dst_size)
+    # Given src and dst points, calculate the perspective transform matrix
+    M = cv2.getPerspectiveTransform(src, dst)
+    # Warp the image using OpenCV warpPerspective()
+    warp = cv2.warpPerspective(img, M, dst_size)
+    return warp
+
+def inv_perspective_warp(img, 
+                     dst_size=(1280,720),
+                     src=np.float32([(0,0), (1, 0), (0,1), (1,1)]),
+                     dst=np.float32([(0.43,0.65),(0.58,0.65),(0.1,1),(1,1)])):
+    img_size = np.float32([(img.shape[1],img.shape[0])])
+    src = src* img_size
+    # For destination points, I'm arbitrarily choosing some points to be
+    # a nice fit for displaying our warped result 
+    # again, not exact, but close enough for our purposes
+    dst = dst * np.float32(dst_size)
+    # Given src and dst points, calculate the perspective transform matrix
+    M = cv2.getPerspectiveTransform(src, dst)
+    # Warp the image using OpenCV warpPerspective()
+    inv_warp = cv2.warpPerspective(img, M, dst_size)
+    return inv_warp
+
+img = cv2.imread("trackDetection-env/test_images/test_.jpg")
+img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(img_gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+warp = perspective_warp(thresh)
+inv_warp = inv_perspective_warp(warp)
+
+
+cv2.imshow('Original', img)
+cv2.imshow('Threshold', thresh)
+cv2.imshow('Warp', warp)
+cv2.imshow('Inv_warp', inv_warp)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
